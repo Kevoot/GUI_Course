f9ded6ff7f250bf609c5113e48c83a82e528567b
U-DESKTOP-D7M69HC\Kevin <kevinholmes88@gmail.com>
2018-12-19 12:50:18 -0500

    Functionality done, fixing up visuals now

--- final/js/main.js
+++ final/js/main.js
@@ -1,18 +1,19 @@
 function createBoard() {
-    let boardStr = "";
-    for (let j = 0; j < BOARD_HEIGHT; j++) {
     let str = '<div id="container"><div class="col-md-12">';
     for (let i = 0; i < BOARD_WIDTH; i++) {
+        if (i === 5 || i === 10) {
+            str += '<div class="holder double-holder boardHolder" id="holder' + i + '"></div>';
+        }
+        else {
             str += '<div class="holder boardHolder" id="holder' + i + '"></div>';
+        }
     }
-    str += '</div></div>'
-    boardStr += str;
-}
-    return boardStr;
+    str += '</div>'
+
+    return str;
 }
 
 const BOARD_WIDTH = 15;
-const BOARD_HEIGHT = 2;
 
 let gameBoard = [];
 
@@ -20,6 +21,7 @@ let boxes = [];
 let tileBag = [];
 let curBox;
 let curDraggingLetter = "";
+let curScore;
 
 function init() {
     createTileBag();
@@ -43,6 +45,7 @@ function init() {
     }
 
     fillPlayerTiles();
+    curScore = 0;
 }
 
 function createTileBag() {
@@ -54,8 +57,14 @@ function createTileBag() {
 }
 
 function createGameBoard() {
-    for (let i = 0; i < BOARD_WIDTH * BOARD_HEIGHT; i++) {
-        gameBoard.push({ currentLetter: undefined, previouslySet: false });
+    for (let i = 0; i < BOARD_WIDTH; i++) {
+        // Set slots 5 and 10 as double word score
+        let multiplier = false;
+        if (i == 5 || i == 10) {
+            multiplier = true;
+        }
+
+        gameBoard.push({ currentLetter: undefined, previouslySet: false, multiplier: multiplier });
     }
 }
 
@@ -64,6 +73,11 @@ function handleDragStart(evt) {
 
     setTimeout(() => this.className = "invisible", 0);
     curBox = this;
+
+    let holderNumber = parseInt(evt.target.id.substring(6));
+    if (!isNaN(holderNumber)) {
+        gameBoard[holderNumber].currentLetter = undefined;
+    }
 }
 
 function handleDragEnd(evt) {
@@ -75,28 +89,64 @@ function handleDragOver(e) {
 }
 
 function handleDragEnter(e) {
-    if (curBox !== undefined) {
+    if (curBox !== undefined && this.children.length < 1) {
         e.preventDefault()
         this.className += " hovered"
     }
 }
 
 function handleDragLeave(evt) {
-    this.className = "holder"
+    // If the holder already has something in it, don't allow.
+    if (this.children.length > 0) {
+        return;
+    }
+
+    holderNumber = parseInt(evt.target.id.substring(6));
+
+    // If this holder is a board space and is a double word tile then
+    // use the separate class
+    if (!isNaN(holderNumber)) {
+        if (holderNumber === 5 || holderNumber === 10) {
+            this.className = "double-holder";
+        }
+        else {
+            this.className = "holder";
+        }
+    }
+    else {
+        this.className = "holder";
+    }
 }
 
 function HandleDrop(evt) {
+    // If the holder already has something in it, don't allow.
+    if (this.children.length > 0) {
+        return;
+    }
+
+    this.append(curBox);
+    curBox.className = "box";
+
     if (curBox !== undefined) {
-        this.className = "holder"
-        this.append(curBox);
-        curBox.className = "box";
         let holderNumber;
+
         if (evt.target.id.charAt(0) === 'p') {
-            holderNumber = evt.target.id.substring(12);
+            this.className = "holder"
+            holderNumber = parseInt(evt.target.id.substring(12));
         }
         else {
-            holderNumber = evt.target.id.substring(6);
+            holderNumber = parseInt(evt.target.id.substring(6));
+
+            // If this holder is a board space and is a double word tile then
+            // use the separate class
+            if (holderNumber === 5 || holderNumber === 10) {
+                this.className = "double-holder";
+            }
+            else {
+                this.className = "holder"
+            }
         }
+
         gameBoard[holderNumber].currentLetter = $(curBox).attr("letter");
         curBox = undefined;
     }
@@ -107,7 +157,7 @@ function fillPlayerTiles() {
     for (let box of boxes) {
         r = Math.floor(Math.random() * tileBag.length);
         let letter = tileBag.splice(r, 1)[0];
-        box.style  = 
+        box.style =
             "background-image: url('./img/Scrabble_Tile_" + letter + ".jpg'); background-size: cover;";
         $(box).attr("letter", letter);
     }
@@ -119,7 +169,7 @@ $(document).on('click', '#submit', () => {
     let resultWord = "";
 
     // Find the first populated entry
-    for (let i = 0; i < BOARD_WIDTH * BOARD_HEIGHT; i++) {
+    for (let i = 0; i < BOARD_WIDTH; i++) {
         if (gameBoard[i].currentLetter && !gameBoard[i].previouslySet) {
             startIndex = i;
             break;
@@ -132,7 +182,7 @@ $(document).on('click', '#submit', () => {
     }
     else {
         // Scan until the last letter is found
-        for (let i = startIndex; i < BOARD_WIDTH * BOARD_HEIGHT; i++) {
+        for (let i = startIndex; i < BOARD_WIDTH; i++) {
             if (gameBoard[i].currentLetter) {
                 resultWord += gameBoard[i].currentLetter;
                 endIndex = i;
@@ -145,8 +195,8 @@ $(document).on('click', '#submit', () => {
         // Do some error checking now, previously played words shouldn't be factored into this
         // This isn't strictly necessary when the board is a singular row, but in case it was
         // 2D, this would be necessary.
-        if (endIndex !== (BOARD_WIDTH * BOARD_HEIGHT) - 1) {
-            for (let i = endIndex + 1; i < BOARD_WIDTH * BOARD_HEIGHT; i++) {
+        if (endIndex !== (BOARD_WIDTH) - 1) {
+            for (let i = endIndex + 1; i < BOARD_WIDTH; i++) {
                 if (gameBoard[i].currentLetter && !gameBoard[i].previouslySet) {
                     handleTilesDisconnectedError();
                     return;
@@ -177,15 +227,38 @@ function completeTurn(start, end) {
         box.removeEventListener("dragstart", handleDragStart);
         box.removeEventListener("dragend", handleDragEnd);
 
-        
+
     }
+    updateScore(calculateWordScore(start, end));
     dealTiles();
+}
+
+function updateScore(num) {
+    curScore = curScore + num;
+    $('#score').html("Current Score: " + curScore);
+}
+
+function calculateWordScore(start, end) {
+    let multiplierFlag = false;
+    let wordTotal = 0;
+    for (let i = start; i <= end; i++) {
+        wordTotal += tileConfig[gameBoard[i].currentLetter].val;
+        if (gameBoard[i].multiplier) {
+            multiplierFlag = true;
+        }
+    }
+
+    if (multiplierFlag) {
+        wordTotal = wordTotal * 2;
+    }
+
+    return wordTotal;
 }
 
 function dealTiles(replace) {
     for (let i = 0; i < 7; i++) {
         let holder = document.getElementById('playerHolder' + i);
-        if(replace) {
+        if (replace) {
             holder.innerHTML = '';
         }
         if (holder.children.length < 1) {
